96b2b35d59d1dc05284a2bf66283b68ef615d24c
Henry Lee <hugh.q.lee@gmail.com>
2020-08-05 16:22:30 +0900

    Postgres Practice 2

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "_10#j4xtpcj7s2_a-q(k&%qxsgq!gn6(l+2-^obn)=4@(&f@a8"
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
 
 # Application definition
